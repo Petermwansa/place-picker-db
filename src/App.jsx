13f7abdccca54dd58e72fb4f82b322493d2b7167
Petermwansa <mwansapeter800@gmail.com>
2024-06-19 09:24:41 +0300

    Do the CRUD on the data by implementing all the http requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
-import { updateUserPlaces } from './http.js';
+import { updateUserPlaces, fetchUserPlaces } from './http.js';
+import Error from './components/Error.jsx';
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
-
+  const [fetching, setisFetching] = useState(false);
+  const [error, setError] = useState();
+  const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+
+
+  // here we are fetching for the places 
+  useEffect(() => {
+    async function fetchPlaces() {
+      setisFetching(true)
+      try {
+        const places = await fetchUserPlaces()
+        setUserPlaces(places);
+      } catch (error) {
+        setError({
+          message: error.message || "Fialed to search the user places"
+        })
+      }
+      setisFetching(false)
+    }
+
+
+    fetchPlaces();
+  }, [])
+
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -25,6 +50,7 @@ function App() {
 
   // this function can be used as an async function because we are just using it as an event listener 
   async function handleSelectPlace(selectedPlace) {
+    // await updateUserPlaces([selectedPlace, ...userPlaces])
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -38,7 +64,8 @@ function App() {
     try {
       await updateUserPlaces([selectedPlace, ...userPlaces])
     } catch (error) {
-      
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({ message: error.message || 'Failed to update the places'})
     }
   }
 
@@ -47,11 +74,32 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
 
+    try {
+      await updateUserPlaces(userPlaces.filter((place) => place.id !== selectedPlace.current.id))
+    } catch (error) {
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({
+        message: error.message || 'Failed to delete a place'
+      })
+    }
+
     setModalIsOpen(false);
-  }, []);
+  }, [userPlaces]);
+
+
+  function handleError() {
+    setErrorUpdatingPlaces(null);
+  }
 
   return (
     <>
+      <Modal open={errorUpdatingPlaces} onClose={handleError}>
+        { errorUpdatingPlaces && (
+          <Error title='An error occered'  message={errorUpdatingPlaces.message}/>
+        )
+        }      
+      </Modal>
+      
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
@@ -68,12 +116,17 @@ function App() {
         </p>
       </header>
       <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText="Select the places you would like to visit below."
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
+        {error && <Error title='An error occured' message={error.message} /> }
+        {! error && (
+          <Places
+            title="I'd like to visit ..."
+            isLoading={fetching}
+            loadingText="Fetching your added places"
+            fallbackText="Select the places you would like to visit below."
+            places={userPlaces}
+            onSelectPlace={handleStartRemovePlace}
+          />
+        )}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
--- src/http.js
+++ src/http.js
@@ -1,52 +1,4 @@
-// export default async function fetchAvailablePlaces() {
-//     const response = await fetch("http://localhost:3000/places");
-//     const resData = await response.json();
-
-//     if (!response.ok) {
-//       throw new Error('failed to fetch the places');
-//     }
-//     return resData.places;
-// }
-
-// // this function will be responsible for updating the UserActivation's places in the db 
-// export async function updateUserPlaces() {
-//     // we add the second arg {} to the fetch which allows you to config the outgoing req 
-//     const response = await fetch('http://localhost:3000/user-places', {
-//         method: 'PUT',
-//         body: JSON.stringify({places: places}),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     });
-
-//     const resData = await response.json();
-
-//     if (!response.ok) {
-//         throw new Error('Failed to update the user data');
-//     }
-
-//     return resData.message;
-// }
-
-
-// // we put this code here so that we can use it in other places that potentially would need it 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// this function will fetch for the available places 
 export default async function fetchAvailablePlaces() {
     const response = await fetch('http://localhost:3000/places');
     const resData = await response.json();
@@ -56,9 +8,27 @@ export default async function fetchAvailablePlaces() {
     }
   
     return resData.places;
+}
+
+
+
+// this function will fetch for the user selected places 
+export async function fetchUserPlaces() {
+  const response = await fetch('http://localhost:3000/user-places');
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch user places');
   }
-  
-  export async function updateUserPlaces(places) {
+
+  return resData.places;
+}
+
+
+
+// this function will be responsible for updating the UserActivation's places in the db 
+export async function updateUserPlaces(places) {
+    // we add the second arg {} to the fetch which allows you to config the outgoing req 
     const response = await fetch('http://localhost:3000/user-places', {
       method: 'PUT',
       body: JSON.stringify({ places }),
@@ -74,9 +44,11 @@ export default async function fetchAvailablePlaces() {
     }
   
     return resData.message;
-  }
+}
 
 
+
+// we put this code here so that we can use it in other places that potentially would need it 
 
 
 
